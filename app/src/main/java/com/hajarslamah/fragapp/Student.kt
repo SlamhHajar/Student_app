74d3f7098ae65f64c5a0e5bed4279d8f17865a0c
Hajar Salamah <programer4455@gmail.com>
2020-10-27 16:29:15 +0300

    Exam

--- app/src/main/java/com/hajarslamah/fragapp/Student.kt
+++ app/src/main/java/com/hajarslamah/fragapp/Student.kt
@@ -1,4 +1,9 @@
 package com.hajarslamah.fragapp
 
-class Student {
-}
+import java.util.*
+
+data class Student (val id : UUID = UUID.randomUUID(),
+                    var num:Int=0,
+                    var name: String="",
+                    var pass:Boolean=false
+)
--- app/src/main/java/com/hajarslamah/fragapp/StudentViewModel.kt
+++ app/src/main/java/com/hajarslamah/fragapp/StudentViewModel.kt
@@ -2,13 +2,14 @@ package com.hajarslamah.fragapp
 
 import androidx.lifecycle.ViewModel
 
-class StudentListView:ViewModel() {
+class StudentViewModel:ViewModel() {
+
     val students= mutableListOf<Student>()
     init {
         for (i in 0 until 200) {
          val student=Student()
 
-            student.name= "Hajar" + i
+            student.name= "Student"+ i
             student.num=i
                student.pass=i % 2 ==1
             students+=student
